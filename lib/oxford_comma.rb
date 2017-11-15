7998b4f45b9ca61b19da41a0fac2d7e247de1925
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-15 03:30:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,17 @@
 def oxford_comma(array)
-  if array.length == 1
-    array.join
-  elsif array.length == 2
-    array.join(" and ")
-  elsif array.length ==3
-    new_array = array.insert(2, "and")
-    string = new_array.join(", ")
-
-
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(" and ")
+    elsif array.length ==3
+      new_array = array.insert(array.length-1, "and ")
+      string = new_array.join(", ")
+      new_string = string.sub! ' , ', ' '
+      new_string
+    elsif array.length ==3
+      new_array = array.insert(array.length-1, "and ")
+      string = new_array.join(", ")
+      new_string = string.sub! ' , ', ' '
+      new_string
+    end
   end
-end
